7a4c6cb3799fc416707754e256955c69954934ca
Aldous Roy <aldousroy007@gmail.com>
2024-08-29 16:07:40 +0530

    tailwindcss installed

--- server/server.js
+++ server/server.js
@@ -1,2 +1,24 @@
-const express = require("express")
-const PORT =4000;
+const express = require('express')
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.post('/login',(req,res)=>{
+    const {name,email}=req.body;
+    console.log(`Received data: Name = ${name}, Email = ${email}`);
+
+    res.json({ message: 'Data received successfully', receivedData: req.body });
+});
+
+app.get('/', (req, res) => {
+  res.send('This is a start point')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
