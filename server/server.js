588a2e09973851552fe7a017754100a04cccaeec
Aldous Roy <aldousroy@Aldouss-MacBook-Air.local>
2024-09-01 03:33:38 +0530

    DB init

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+const mongoose =require('mongoose')
 const app = express()
 const port = 3000
 
+const mongourl="mongodb://localhost:27017/new_login"
+
 app.use(cors())
 app.use(bodyParser.json())
+
+const DBconnect= async()=>{
+  try{
+    await mongoose.connect(mongourl)
+    console.log("DB connected")
+  }
+  catch(error){
+    console.error("DB connection failed")
+    process.exit(1);//fail safe methoid
+  }
+}
+DBconnect();
 
 app.post('/login',(req,res)=>{
     const {name,email}=req.body;
